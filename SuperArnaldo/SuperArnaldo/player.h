3bf0417a32920671d38b6a85bc21dd963c57815d
mmilanezi <mmgrassi@outlook.com>
2021-04-29 09:57:51 -0300

    Versão 0.0.0.1

---
+++ SuperArnaldo/SuperArnaldo/player.h
@@ -0,0 +1,20 @@
+#pragma once
+class player
+{
+
+	void pula() {
+		//metodo que executa ação de pular
+	}
+
+	void anda_left() {
+		//metodo que executa ação de andar para a esquerda
+		
+	}
+
+	void anda_rigth() {
+		//metodo que executa ação de andar para a direita
+
+	}
+
+};
+
